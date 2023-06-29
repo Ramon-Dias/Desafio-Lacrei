7139801ea05daaa68adde0c3b951b65283ab810f
Ramon-Dias <ramondf13@gmail.com>
2023-06-28 23:17:39 -0300

    feat: header,footer,home com html estruturado, imagens dos respectivos adicionadas

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,8 +1,20 @@
+import { Link } from "react-router-dom";
+
 function Footer() {
     return (
-      <div>
-        Footer
-      </div>
+      <footer>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/usuario">Usuario</Link>
+          <Link to="/profissional">Profissional</Link>
+        </nav>
+        <div>
+          <img src="../../assets/FacebookLogo.svg" alt="Facebook Logo"/>
+          <img src="../../assets/InstagramLogo.svg" alt="Instagram Logo"/>
+          <img src="../../assets/LinkedinLogo.svg" alt="Linkedin Logo"/>
+        </div>
+        <p>Desafio Front-end Lacrei</p>
+      </footer>
     );
   }
   
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,15 @@
+import { Link } from "react-router-dom";
+
 function Header() {
     return (
-      <div>
-        Header
-      </div>
+      <header>
+        <h1>Lacrei</h1>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/usuario">Usuario</Link>
+          <Link to="/profissional">Profissional</Link>
+        </nav>
+      </header>
     );
   }
   
