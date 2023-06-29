1b194a25d0007959a3c8d98e7e77bab8568bf3bf
Ramon-Dias <ramondf13@gmail.com>
2023-06-28 21:32:14 -0300

    feat: componentes criados, ajustes nas rotas para melhor funcionamento com os novos componentes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
 
 
 function App() {
   return (
     <div className="App">
-        App
+        <Header/>
+        <Outlet/>
+        <Footer/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -6,19 +6,28 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Usuario from './pages/usuario/Usuario';
 import Profissional from './pages/profissional/Profissional';
+import App from './App';
+import Error from './components/error/Error';
 
 const router = createBrowserRouter([
   {
     path:"/",
-    element: <Home/>
-  },
-  {
-    path:"usuario",
-    element:<Usuario/>
-  },
-  {
-    path:"profissional",
-    element:<Profissional/>
+    element: <App/>,
+    errorElement: <Error/>,
+    children: [
+      {
+        path:"/",
+        element:<Home/>
+      },
+      {
+        path:"usuario",
+        element:<Usuario/>
+      },
+      {
+        path:"profissional",
+        element:<Profissional/>
+      }
+    ]
   }
 ])
 
